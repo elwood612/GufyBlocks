1348e380f130ed13ae53da9bfd4cc7d2ba70c7a5
Ray Herrly <ray.herrly@gmail.com>
2025-05-14 10:14:50 +0200

    Fixed crashes on startup

--- src/main/java/com/github/elwood612/gufyblocks/blocks/blockUtil/GufyProperties.java
+++ src/main/java/com/github/elwood612/gufyblocks/blocks/blockUtil/GufyProperties.java
@@ -1,8 +1,10 @@
 package com.github.elwood612.gufyblocks.blocks.blockUtil;
 
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 
 import static com.github.elwood612.gufyblocks.util.GufyUtil.copyPropertiesOf;
 
@@ -25,6 +27,22 @@ public class GufyProperties {
     public static final BlockBehaviour.Properties MUSHROOM_FRAMED_GLASS = copyPropertiesOf(Blocks.GLASS_PANE).mapColor(MapColor.DIRT);
     public static final BlockBehaviour.Properties LEADED_GLASS = copyPropertiesOf(Blocks.GLASS_PANE).mapColor(MapColor.STONE).strength(0.3f, 3.0f);
     public static final BlockBehaviour.Properties WHITE_FRAMED_TERRACOTTA = copyPropertiesOf(Blocks.WHITE_TERRACOTTA).strength(2.0f, 6.0f);
-    public static final BlockBehaviour.Properties WOODEN_SEAT = copyPropertiesOf(Blocks.WHITE_BED).mapColor(MapColor.WOOD);
+    public static final BlockBehaviour.Properties WOODEN_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(MapColor.WOOD).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties WHITE_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.WHITE).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties LIGHT_GRAY_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.LIGHT_GRAY).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties GRAY_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.GRAY).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties BLACK_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.BLACK).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties BROWN_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.BROWN).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties RED_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.RED).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties ORANGE_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.ORANGE).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties YELLOW_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.YELLOW).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties LIME_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.LIME).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties GREEN_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.GREEN).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties CYAN_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.CYAN).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties LIGHT_BLUE_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.LIGHT_BLUE).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties BLUE_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.BLUE).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties PURPLE_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.PURPLE).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties MAGENTA_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.MAGENTA).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
+    public static final BlockBehaviour.Properties PINK_SEAT = copyPropertiesOf(Blocks.OAK_SLAB).mapColor(DyeColor.PINK).strength(0.2f).ignitedByLava().pushReaction(PushReaction.DESTROY);
 
 }
