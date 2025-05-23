c0b76a5084bf9afa2dfef8dec0c59da7442fddba
elwood612 <30651423+elwood612@users.noreply.github.com>
2025-05-23 06:15:49 +0200

    Fern color fix

--- src/main/java/com/github/elwood612/gufyblocks/util/GufyTintHandler.java
+++ src/main/java/com/github/elwood612/gufyblocks/util/GufyTintHandler.java
@@ -2,6 +2,7 @@ package com.github.elwood612.gufyblocks.util;
 
 import com.github.elwood612.gufyblocks.GufyBlocks;
 import net.minecraft.client.renderer.BiomeColors;
+import net.minecraft.world.level.GrassColor;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
@@ -13,7 +14,9 @@ public class GufyTintHandler
     public static void registerBlockColorHandlers(RegisterColorHandlersEvent.Block event) {
         event.register((state, level, pos, tintIndex) ->
                 level != null && pos != null ? BiomeColors.getAverageFoliageColor(level, pos) : -12012264,
-                GufyUtil.getGufyBlock("potted_large_fern"),
                 GufyUtil.getGufyBlock("potted_vine"));
+        event.register((state, level, pos, tintIndex) ->
+                        level != null && pos != null ? BiomeColors.getAverageGrassColor(level, pos) : GrassColor.getDefaultColor(),
+                GufyUtil.getGufyBlock("potted_large_fern"));
     }
 }
